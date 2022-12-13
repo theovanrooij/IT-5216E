7850e4d1932097aedae07827634d1ce517dce264
Méline Dang <84090602+MelineDang@users.noreply.github.com>
2022-12-13 15:11:32 +0100

    newquizpage : initialisation
    ajout du input pour le nom, formulaire de saisie avec le bouton et binding

--- quiz-ui/src/App.vue
+++ quiz-ui/src/App.vue
@@ -9,6 +9,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/new-quiz-page">NewQuizPage</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
---
+++ quiz-ui/src/views/NewQuizPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="new-quiz-page">
+    <h1>New Quiz Page</h1>
+     
+    <p> <br><br><br><br><br><br><br><br><br><br><br><br>Saisissez votre nom :<br>
+    <input type="text" v-model="username" id="name" name="name" size="10">
+    <button class="favorite styled" type="button"> GO!</button>
+  </p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<script>
+export default {
+  name: "NewQuizPage",
+  data() {
+    return { 
+      username : ''
+    };
+  }
+}
+</script>
