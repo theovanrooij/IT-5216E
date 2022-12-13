95431621316a62e5d670860f9f61570b24f9b5dc
Méline Dang <84090602+MelineDang@users.noreply.github.com>
2022-12-13 14:31:06 +0100

    front : initialisation
    dossier quizz-ui, composant HomePage

---
+++ quiz-ui/src/views/HomePage.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>Home page</h1>
+
+  <div v-for="scoreEntry in registeredScores" v-bind:key="scoreEntry.date">
+  {{ scoreEntry.playerName }} - {{ scoreEntry.score }}
+  <router-link to="/start-new-quiz-page">Démarrer le quiz !</router-link>
+
+</div>
+
+</template>
+
+<script>
+{/* <style> <p> Test </p></style> */}
+import quizApiService from "@/services/QuizApiService";
+
+export default {
+  name: "HomePage",
+  data() {
+    return { 
+      registeredScores : []
+    };
+  },
+  components: {
+
+  },
+  async created() {
+		// console.log("Composant Home page 'created'");
+	  var quizInfoPromise = quizApiService.getQuizInfo();
+	  var quizInfoApiResult = await quizInfoPromise;
+    this.registeredScores = quizInfoApiResult.data.scores;
+  }
+};
+</script>
